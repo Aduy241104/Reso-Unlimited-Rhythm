7ed25c2568b48a36d2e721c4c2d29e35ee82a947
Bui Anh Duy <aduy8774@gmail.com>
2026-04-29 08:52:42 +0700

    feat: add configuration files for Cloudinary, CORS, MongoDB, and Redis; implement server setup

---
+++ Backend/src/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import dotenv from "dotenv";
+import route from "./router/index.js";
+import morgan from "morgan";
+import connectMongose from "./config/db.js";
+import cors from "cors";
+import corsOptions from "./config/corsConfig.js";
+import cookieParser from "cookie-parser";
+import http from "http";
+import redisClient from "./config/redisConfig.js";
+import {
+    globalErrorHandler,
+    notFoundHandler,
+} from "./middlewares/error.middleware.js";
+
+
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connectMongose();
+redisClient.connect().then(() => {
+    console.log("✅ Kết nối Redis thành công");
+}).catch((err) => {
+    console.error("❌ Lỗi kết nối Redis:", err);
+});
+
+app.use(morgan("combined"));
+
+route(app);
+
+app.get("/test", async (req, res) => { res.json("hello") });
+
+app.use(notFoundHandler);
+app.use(globalErrorHandler);
+
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+    console.log(`🚁 Server + Socket.IO đang chạy tại http://localhost:${PORT}`);
+});
