8dc379042a44858fba3b2f8a8760df2d374df477
Bui Anh Duy <aduy8774@gmail.com>
2026-09-13 10:15:32 +0700

    fix: add missing origin to allowed CORS origins

--- Backend/src/config/corsConfig.js
+++ Backend/src/config/corsConfig.js
@@ -14,7 +14,8 @@ const allowedOrigins = [
     "http://192.168.1.20:8081",
     "exp://192.168.1.20:8081",
     "https://tsw6wlqc-5174.asse.devtunnels.ms",
-    "https://tsw6wlqc-5173.asse.devtunnels.ms"
+    "https://tsw6wlqc-5173.asse.devtunnels.ms",
+    "https://reso-unlimited-rhythm.vercel.app"
 ];
 
 export const corsOptions = {
